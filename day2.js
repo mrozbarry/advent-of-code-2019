c6ade33447ebc54e23c012ed0a1bd069135290da
Alex Barry <alex.barry@gmail.com>
2019-12-02 15:13:40 -0500

    Day 2 challenge

---
+++ day2.js
@@ -0,0 +1,33 @@
+const path = require('path');
+const intcode = require('./lib/intcode');
+
+const firstProgram = path.resolve(__dirname, 'input.program');
+
+const run = (opcodes, noun, verb) => {
+  opcodes[1] = noun;
+  opcodes[2] = verb;
+  const memory = intcode.run(opcodes);
+  return memory[0];
+}
+
+// Original (3931283)
+intcode.loadFromFile(firstProgram)
+  .then((opcodes) => run(opcodes, 12, 2))
+  .then(console.log);
+
+// // Part II (noun=69, verb=79)
+// intcode.loadFromFile(firstProgram)
+//   .then((opcodes) => {
+//     const desiredOutput = 19690720;
+//     for(let noun = 0; noun < opcodes.length; noun++) {
+//       for(let verb = 0; verb < opcodes.length; verb++) {
+//         if (run(opcodes, noun, verb) === desiredOutput) {
+//           return {
+//             noun,
+//             verb,
+//           };
+//         }
+//       }
+//     }
+//   })
+//   .then(console.log);
