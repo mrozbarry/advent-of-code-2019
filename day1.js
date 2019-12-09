119b510d2f4b44660854fc02916dda575fcf9ab4
Alex Barry <alex.barry@gmail.com>
2019-12-09 08:21:45 -0500

    Better day1 output

--- day1.js
+++ day1.js
@@ -5,6 +5,7 @@ const fuel = require('./lib/fuel');
 const sum = arr => arr.reduce((memo, v) => memo + v, 0);
 
 fuel.readMassesFrom(path.resolve(__dirname, 'day1.lst'))
-  .then(masses => masses.map(m => fuel.fromMassAndFuel(m)))
-  .then(sum)
-  .then(console.log); // 3406432 (without fuel), 5106777 (with fuel)
+  .then(masses => {
+    console.log('without fuel', sum(masses.map(m => fuel.fromMass(m))));
+    console.log('including fuel', sum(masses.map(m => fuel.fromMassAndFuel(m))));
+  }); // 3406432 (without fuel), 5106777 (with fuel)
