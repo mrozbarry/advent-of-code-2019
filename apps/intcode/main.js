3ebc7ef573b3d372ec8c5e5fd6354d0a3a27c8c6
Alex Barry <alex.barry@gmail.com>
2019-12-14 18:33:53 -0500

    Improvements to hyperapp intcode debugger

--- apps/intcode/main.js
+++ apps/intcode/main.js
@@ -1,6 +1,7 @@
+import '@babel/polyfill';
 import { app, h } from 'hyperapp';
 import * as maybe from '../../lib/maybe';
-import { step } from '../../lib/intcode';
+import { loadFromString, step, isProgramDone } from '../../lib/intcode2';
 
 const decodeValue = e => e.target.value;
 
@@ -8,6 +9,8 @@ const Init = () => (
   {
     code: '',
     program: maybe.nothing(),
+    inputsBuffer: [],
+    outputs: [],
   }
 );
 
@@ -21,95 +24,96 @@ const CodeUpdate = (state, code) => ({
 
 const ProgramLoad = (state) => ({
   ...state,
-  program: maybe.just({
-    memory: [
-      state.code.trim().split(',').map(Number),
-    ],
-    address: 0,
+  program: maybe.just(loadFromString(state.code)),
+});
+
+const ProgramUnload = state => ({ ...state, program: maybe.nothing() });
+
+const ProgramResult = (state, program) => ({
+  ...state,
+  program: program ? maybe.just(program) : maybe.nothing(),
+});
+
+const inputFromPrompt = () => ({
+  next: () => ({
+    done: false,
+    value: window.prompt('Program is expecting input'),
   }),
 });
 
-const stepMemoryAndAddress = (memory, address) => {
-  return step(memory, address, {
-    input: () => Promise.resolve(window.prompt('Program is expecting input')),
+const StepFX = async (dispatch, { program, inputsBuffer }) => {
+  const nextProgram = await step(program, {
+    input: inputFromPrompt(),
+    //input: {
+      //next: () => {
+        //const value = inputsBuffer.shift();
+        //return {
+          //value,
+          //done: false,
+        //}
+      //},
+    //},
     output: (v) => window.alert(`Program output: ${v}`),
   });
+  dispatch(ProgramResult, nextProgram);
 };
-
-const ProgramResult = (state, { memory, address }) => ({
-  ...state,
-  program: maybe.match({
-    [maybe.Nothing]: () => maybe.just({ memory: [memory], address }),
-    [maybe.Just]: (program) => {
-      return maybe.just({
-        memory: [...program.memory, memory],
-        address,
-      });
-    },
-  }, state.program),
-});
-
-const StepFX = (dispatch, { memory, address }) => {
-  stepMemoryAndAddress(memory, address)
-    .then(result => dispatch(
-      ProgramResult,
-      {
-        memory: result[0],
-        address: result[1],
-      },
-    ))
-};
-const Step = ({ memory, address }) => [StepFX, { memory, address }];
+const Step = (program) => [StepFX, { program }];
 
 const ProgramStep = (state) => maybe.match({
   [maybe.Nothing]: () => state,
   [maybe.Just]: (program) => {
-    const prevMemory = program.memory[program.memory.length - 1];
-    if (prevMemory[program.address] === 99) {
+    if (program.memory[program.address] === 99) {
       return state;
     }
 
-    return [state, Step({ memory: prevMemory, address: program.address })];
+    return [state, Step(program)];
   },
 }, state.program)
 
-const viewCode = ({ program }) => {
-  return maybe.match({
-    [maybe.Just]: (currentProgram) => {
-      const memory = currentProgram.memory[currentProgram.memory.length - 1];
-      return h(
-        'div',
-        {
-          class: 'program',
-        },
-        memory.map((v, addr) => h(
-          'div',
-          {
-            class: {
-              block: true,
-              highlight: addr === currentProgram.address,
-            },
-          },
-          v.toString()
-        )),
-      );
-    },
-    [maybe.Nothing]: () => h('div', { class: 'block' }, [
-      h('strong', null, 'No program loaded'),
-    ]),
-  }, program)
-};
+const AddInput = (state, integer) => ({ ...state, inputsBuffer: [integer].concat(state.inputsBuffer) });
+
+const viewInputs = ({ inputsBuffer }) => h('div', null, [
+
+]);
+
+const viewOutputs = ({ outputs }) => h('div', null, ['Outputs', h('ul', null, outputs.map(v => h('li', null, v)))]);
 
 app({
   init: Init,
   view: state => {
     return h('div', null, [
-      h('div', null, [
-        h('textarea', { value: state.code, oninput: [CodeUpdate, decodeValue] }),
-      ]),
-      h('button', { onclick: ProgramLoad }, 'Load program'),
-      h('button', { onclick: ProgramStep }, 'Step program'),
-      viewCode(state),
+      maybe.match({
+        [maybe.Just]: (program) => {
+          return h('section', null, [
+            h('button', { onclick: ProgramStep, disabled: isProgramDone(program) }, 'Step program'),
+            h('button', { onclick: ProgramUnload }, 'Unload program'),
+            h(
+              'div',
+              {
+                class: 'program',
+              },
+              program.memory.map((v, addr) => h(
+                'div',
+                {
+                  class: {
+                    block: true,
+                    highlight: addr === program.address,
+                  },
+                },
+                v.toString()
+              )),
+            ),
+            viewOutputs(state),
+          ]);
+        },
+        [maybe.Nothing]: () => h('section', null, [
+          h('div', null, [
+            h('textarea', { value: state.code, oninput: [CodeUpdate, decodeValue] }),
+          ]),
+          h('button', { onclick: ProgramLoad }, 'Load program'),
+          h('strong', null, 'No program loaded'),
+        ]),
+      }, state.program),
     ]);
   },
   node: document.getElementById('app'),
